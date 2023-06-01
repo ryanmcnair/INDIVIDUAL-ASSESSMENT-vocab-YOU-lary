f83a8db6f91800425b4eb07978996b4d03011bd0
ryanmcnair <ryan.mcnair@gmail.com>
2023-05-31 21:24:56 -0500

    create working properly

--- events/formEvents.js
+++ events/formEvents.js
@@ -1,17 +1,32 @@
-// const formEvents = (user) => {
-//   document.querySelector('#form').addEventListener('submit', (e) => {
-//     e.preventDefault();
-//     if (e.target.id.includes('submit-entry')) {
-//       const payload = {
-//         title: document.querySelector('#titleInput').value,
-//         definition: document.querySelector('#definitionInput').value,
-//         language: document.querySelector('#languageSelect').value,
-//         isPublic: true,
-//         time: new Date().toLocaleString,
-//         uid: user.uid,
-//       };
-//     }
-//   });
-// };
+import { createEntry, getEntries, updateEntry } from '../api/entryData';
+import showButtons from '../components/filterButtons';
+import showCards from '../pages/card';
+import clearDom from '../utils/clearDom';
+import filterEvents from './filterEvents';
 
-// export default formEvents;
+const formEvents = (user) => {
+  document.querySelector('#form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.warn('clicked');
+    const payload = {
+      title: document.querySelector('#titleInput').value,
+      definition: document.querySelector('#definitionInput').value,
+      language: document.querySelector('#languageSelect').value,
+      isPublic: true,
+      time: new Date().toLocaleString,
+      uid: user.uid,
+    };
+    createEntry(payload).then(({ name }) => {
+      const patchPayload = { firebaseKey: name };
+      updateEntry(patchPayload).then(() => {
+        getEntries()
+          .then(clearDom())
+          .then(showButtons())
+          .then(filterEvents())
+          .then(showCards);
+      });
+    });
+  });
+};
+
+export default formEvents;
--- utils/startApp.js
+++ utils/startApp.js
@@ -3,18 +3,18 @@ import logoutButton from '../components/logoutButton';
 import navbar from '../components/navbar';
 import navigationEvents from '../events/navigationEvents';
 import { getEntries } from '../api/entryData';
-// import formEvents from '../events/formEvents';
+import formEvents from '../events/formEvents';
 import filterEvents from '../events/filterEvents';
 import showCards from '../pages/card';
 import showButtons from '../components/filterButtons';
 
-const startApp = () => {
+const startApp = (user) => {
   getEntries().then(showCards);
   domBuilder();
   navbar();
   logoutButton();
   navigationEvents();
-  //   formEvents(user);
+  formEvents(user);
   showButtons();
   filterEvents();
 };
